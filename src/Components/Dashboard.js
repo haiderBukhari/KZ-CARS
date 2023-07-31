3bb2d3e397ca53dcbfd1f3544e4942ba371596c4
haiderBukhari <apshaiderbukhari786@gmail.com>
2023-07-31 11:32:38 +0500

    first commitlk

--- src/Components/Dashboard.js
+++ src/Components/Dashboard.js
@@ -191,17 +191,17 @@ export const Dashboard = () => {
   }
   let handlechange = async (name, fromlocation, tolocation, email, date, time, price, status, id, contact) => {
     const datarecord = doc(database, "kz-cars-orders", id)
-    await updateDoc(datarecord, { status: status }).then(data=>{
+    await updateDoc(datarecord, { status: status }).then(data => {
       fetchdata()
-      data.map((arr)=>{
-      if(arr.status === status){
-        setisfound(true)
-      }
-      else{
-        setisfound(false)
-      }
-    })
-    }).catch(err=>console.log(err))
+      data.map((arr) => {
+        if (arr.status === status) {
+          setisfound(true)
+        }
+        else {
+          setisfound(false)
+        }
+      })
+    }).catch(err => console.log(err))
     setisfound(false)
     if (status === 'Approved') {
       const emailParams = {
@@ -267,15 +267,15 @@ export const Dashboard = () => {
     }
   }
   let [isfound, setisfound] = useState(false)
-  let handlechanged = async(e)=>{
+  let handlechanged = async (e) => {
     setisfound(false)
-    dashboard.map((arr)=>{
-      if(arr.status === e.target.value){
+    dashboard.map((arr) => {
+      if (arr.status === e.target.value) {
         setvalidstatus(e.target.value)
         setisfound(true)
         return;
       }
-      else{
+      else {
         setisfound(false)
       }
     })
@@ -290,53 +290,53 @@ export const Dashboard = () => {
         <option value="Completed">Completed</option>
       </select>
       {
-        dashboard.length === 0 ? <EmptyCart /> :  <div className="all">
+        dashboard.length === 0 ? <p className='loading'>{`No Item Found....`}</p> : <div className="all">
           {
             dashboard.map((arr) => (
               validstatus === arr.status && (<>
-              
-              <div className="above">
-                <p className='ps pc-1'><span className='sps'>Date</span>: {arr.data}</p>
-                <p className='ps pc-1'><span className='sps'>Time</span>: {arr.time}</p>
-                <p className='ps pc-1'><span className='sps'>Price</span>: £{arr.price}</p>
-                <div className="data">
-                  <div className="flexs">
-                    <span className='aps'>Name:</span>
-                    <p className='ps'>{arr.name}</p>
+
+                <div className="above">
+                  <p className='ps pc-1'><span className='sps'>Date</span>: {arr.data}</p>
+                  <p className='ps pc-1'><span className='sps'>Time</span>: {arr.time}</p>
+                  <p className='ps pc-1'><span className='sps'>Price</span>: £{arr.price}</p>
+                  <div className="data">
+                    <div className="flexs">
+                      <span className='aps'>Name:</span>
+                      <p className='ps'>{arr.name}</p>
+                    </div>
+                    <div className="flexs">
+                      <span className='aps'>From Location:</span>
+                      <p className='ps'>{arr.fromlocation}</p>
+                    </div>
+                    <div className="flexs">
+                      <span className='aps'>To Location:</span>
+                      <p className='ps'>{arr.tolocation}</p>
+                    </div>
                   </div>
-                  <div className="flexs">
-                    <span className='aps'>From Location:</span>
-                    <p className='ps'>{arr.fromlocation}</p>
+                  <div className="datassss">
+                    <div className="flexs">
+                      <span className='aps'>Email:</span>
+                      <p className='ps'>{arr.email}</p>
+                    </div>
+                    <div className="flexs">
+                      <span className='aps'>Car Status:</span>
+                      <p className='ps'>{arr.status1 ? arr.audi ? "Selected (Audi)" : (arr.mercedes) ? "Selected (Mercedes)" : "Any Selected Vehicle" : "Random Vehicle"}</p>
+                    </div>
+                    <div className="flexs">
+                      <span className='aps'>Contact Number:</span>
+                      <p className='ps'>{arr.Contact}</p>
+                    </div>
                   </div>
-                  <div className="flexs">
-                    <span className='aps'>To Location:</span>
-                    <p className='ps'>{arr.tolocation}</p>
+                  <p className='ps pc-1 pc-2'>Status: <span className='pen'><select onChange={(e) => { handlechange(arr.name, arr.fromlocation, arr.tolocation, arr.email, arr.data, arr.time, arr.price, e.target.value, arr.id, arr.Contact) }} className="akakak" name="" id="">
+                    <option value="Pending" selected={arr.status === 'Pending' ? true : false}>Pending</option>
+                    <option value="Approved" selected={arr.status === 'Approved' ? true : false}>Approved</option>
+                    <option value="Regected" selected={arr.status === 'Regected' ? true : false}>Regected</option>
+                    <option value="Completed" selected={arr.status === 'Completed' ? true : false}>Completed</option>
+                  </select></span></p>
+                  <div className="bnn">
+                    <button onClick={() => { deleterecord(arr.id) }} className='deleterecord'>Delete Record</button>
                   </div>
                 </div>
-                <div className="datassss">
-                  <div className="flexs">
-                    <span className='aps'>Email:</span>
-                    <p className='ps'>{arr.email}</p>
-                  </div>
-                  <div className="flexs">
-                    <span className='aps'>Car Status:</span>
-                    <p className='ps'>{arr.status1 ? arr.audi ? "Selected (Audi)" : (arr.mercedes) ? "Selected (Mercedes)" : "Any Selected Vehicle" : "Random Vehicle"}</p>
-                  </div>
-                  <div className="flexs">
-                    <span className='aps'>Contact Number:</span>
-                    <p className='ps'>{arr.Contact}</p>
-                  </div>
-                </div>
-                <p className='ps pc-1 pc-2'>Status: <span className='pen'><select onChange={(e) => { handlechange(arr.name, arr.fromlocation, arr.tolocation, arr.email, arr.data, arr.time, arr.price, e.target.value, arr.id, arr.Contact) }} className="akakak" name="" id="">
-                  <option value="Pending" selected={arr.status==='Pending'?true:false}>Pending</option>
-                  <option value="Approved" selected={arr.status==='Approved'?true:false}>Approved</option>
-                  <option value="Regected" selected={arr.status==='Regected'?true:false}>Regected</option>
-                  <option value="Completed" selected={arr.status==='Completed'?true:false}>Completed</option>
-                </select></span></p>
-                <div className="bnn">
-                  <button onClick={() => { deleterecord(arr.id) }} className='deleterecord'>Delete Record</button>
-                </div>
-              </div>
               </>)
             ))
           }
